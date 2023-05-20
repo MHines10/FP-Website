44f2b3af3c79cdd1c3e1db7a0a30399ac3d641ea
MHines10 <cd.mhines@gmail.com>
2023-05-20 16:22:45 -0400

    alerts update

--- src/components/Alerts.jsx
+++ src/components/Alerts.jsx
@@ -4,7 +4,7 @@ export default function Alerts({ message, category, flashMessage }) {
   return (
     <div className={`alert alert-${category} alert-dismissible fade show`} role="alert">
             <strong>{message}</strong>
-            {/* <button className='btn-close' onClick={() => flashMessage(null, null)}></button> */}
+            <button className='btn-close' onClick={() => flashMessage(null, null)}></button>
         </div>
   )
 }
