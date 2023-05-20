e86ae84fc812df2e84c70b07a5e9c622bd4d23f0
MHines10 <cd.mhines@gmail.com>
2023-05-20 03:03:14 -0400

    api test

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,7 +19,7 @@ export default function Login(props) {
     myHeaders.append("Content-Type", "application/json");
 
     let response = await fetch(
-        "http://localhost:5000/api/token",
+        "https://cobalt-quick-force.glitch.me/api/token",
       {
         method: "GET",
         headers: myHeaders,
